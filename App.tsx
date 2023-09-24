422ef107fea716f494d8ecb4aa237ad70339d983
DishaMahajan22 <disham2004@gmail.com>
2023-09-24 15:02:50 -0700

    added sign in navigation

--- App.tsx
+++ App.tsx
@@ -1,17 +1,32 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 import { NativeWindStyleSheet } from "nativewind";
+import {NavigationContainer} from '@react-navigation/native'
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import LandingPage from "./pages/landingPage";
+import SignInPage from "./pages/SignInPage";
 
 NativeWindStyleSheet.setOutput({
   default: "native",
 });
 
-
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="LandingPage">
+        <Stack.Screen 
+          name = "Landing"
+          component={LandingPage}
+          options={{
+            headerShown: false
+          }}
+        />
+
+        <Stack.Screen name="SignIn" component={SignInPage}/>
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
---
+++ pages/SignInPage.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+const SignInPage = () => {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'white' }}>
+            <Text>Sign In page</Text>
+        </View>
+    );
+  };
+  
+  export default SignInPage;
