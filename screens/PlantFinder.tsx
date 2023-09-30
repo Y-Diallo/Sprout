0ffe9fcdff0bcc2dd736ffac8cbef5f5c3ab6289
Ingvar2022 <Ingvar20@outlook.com>
2023-09-30 13:54:34 -0700

    Added plant finder UI (should ACTUALLY work this time)

--- screens/PlantFinder.tsx
+++ screens/PlantFinder.tsx
@@ -1,5 +1,9 @@
+import * as Location from "expo-location";
+import { useEffect, useState } from "react";
 import React from 'react';
 import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
+import { fetchPolygonData, handleRecommendations } from "../backend/api";
+
 
 const PlantFinder = () => {
   return (
@@ -93,5 +97,51 @@ const styles = StyleSheet.create({
     borderRadius: 10,
   },
 });
+
+type LocationObjectCoords = {
+  latitude: number;
+  longitude: number;
+};
+
+const [location, setLocation] = useState<LocationObjectCoords | null>(null);
+  const [weatherData, setWeatherData] = useState<any>(null);
+  const [soilData, setSoilData] = useState<any>(null);
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        console.log("Permission to access location was denied");
+        return;
+      }
+
+      let loc = await Location.getCurrentPositionAsync({});
+      setLocation(loc.coords);
+      console.log(
+        "Latitude: " +
+          loc.coords.latitude +
+          " Longitude: " +
+          loc.coords.longitude
+      );
+
+        const ans = await fetchPolygonData(location?.latitude, location?.longitude);
+        console.log("Weather data:", ans.weather);
+        console.log("Soil data:", ans.soil);
+
+        try {
+          setWeatherData(ans.weather);
+          setSoilData(ans.soil);
+          const combinedData = {
+            location: location,
+            weatherData: weatherData,
+            soilData: soilData,
+          };
+          const aiResult = await handleRecommendations(combinedData);
+          console.log(aiResult)
+        } catch (error) {
+          console.error("Error parsing JSON:", error);
+        }
+
+    })();
+  }, []);
 
 export default PlantFinder;
