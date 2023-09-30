c9d17b27484ff6598e4053ed90320efa421ac107
Youssoupha Diallo <youssoupha.d72@gmail.com>
2023-09-30 11:59:16 -0700

    added basic navbar

--- App.tsx
+++ App.tsx
@@ -1,32 +1,47 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
+
 import { NativeWindStyleSheet } from "nativewind";
 import {NavigationContainer} from '@react-navigation/native'
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 import Landing from "./screens/Landing";
 import SignIn from "./screens/SignIn";
+import Navbar from "./components/Navbar/Navbar";
 
 NativeWindStyleSheet.setOutput({
   default: "native",
 });
 
+type RootStackParamList = {
+  Landing: undefined;
+  SignIn: undefined;
+  SignUp: undefined;
+  PlantInformation: { userId: string, inGarden: boolean };
+};
+
 export default function App() {
-  const Stack = createNativeStackNavigator();
+  const Stack = createNativeStackNavigator<RootStackParamList>();
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="LandingPage">
-        <Stack.Screen 
-          name = "Landing"
-          component={Landing}
-          options={{
-            headerShown: false
-          }}
-        />
+    <>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Landing">
+          <Stack.Screen 
+            name = "Landing"
+            component={Landing}
+            options={{
+              headerShown: false
+            }}
+          />
 
-        <Stack.Screen name="SignIn" component={SignIn}/>
+          <Stack.Screen name="SignIn" component={SignIn}/>
 
-      </Stack.Navigator>
-    </NavigationContainer>
+        </Stack.Navigator>
+      </NavigationContainer>
+      <Navbar navigation={undefined}/>
+    </>
   );
 }
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,9 +1,33 @@
-import { View, Text, StatusBar } from 'react-native';
-const Navbar = () => {
+import { View, Text, TouchableOpacity } from 'react-native';
+
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+
+type NavbarProps = {
+  navigation : any;
+}
+const Navbar = ({navigation}: NavbarProps) => {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'white' }}>
-        <Text>Landing Page</Text>
-        <StatusBar hidden={true} />
+      <View className='absolute bottom-0 w-full'>
+        <View className='flex flex-row justify-center'>
+          <View className='grow bg-green-600 flex items-center h-32 mt-auto justify-center'>
+            <TouchableOpacity onPress={()=> navigation.navigate('PlantFinder')}>
+              <Ionicons name="home-outline" size={60} color="#fff"/>
+            </TouchableOpacity>
+          </View>
+          <View className='grow flex items-center'>
+            <View className='bg-green-600 w-full h-52 flex items-center rounded-t-[40%] pt-6'>
+              <TouchableOpacity onPress={()=> navigation.navigate('Picture')}>
+                <Ionicons name="image-outline" size={100} color="#fff"/>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View className='grow bg-green-600 flex items-center h-32 mt-auto justify-center' >
+            <TouchableOpacity onPress={()=> navigation.navigate('Garden')}>
+              <Ionicons  name="earth-outline" size={60} color="#fff"/>
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
     );
   };
